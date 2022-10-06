bb4799104821b873c913cb3767a56914d6efa809
Gi Deok Jang <deok@Gis-MacBook-Pro.local>
2022-10-06 17:17:21 +0900

    Set Sidebar html structure
    set wrapper, sidebar
    
    	modified:   components/Layout/SideBar/SideBar.tsx

--- components/Layout/SideBar/SideBar.tsx
+++ components/Layout/SideBar/SideBar.tsx
@@ -2,5 +2,11 @@ import React from "react";
 import styled from "styled-components";
 
 export const SideBar: React.FC = () => {
-  return <div></div>;
+  return (
+    <div id="wrapper">
+      <div id="sidebar">
+        <>children</>
+      </div>
+    </div>
+  );
 };

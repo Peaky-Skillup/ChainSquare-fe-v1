c0eef03438ddeadf70bd243dc6cf360952c72fa8
dead-pool-aka-wilson <dead.pool.aka.wilson@gmail.com>
2022-10-07 16:12:40 +0900

    Set SideBar for Desktop View
    - add Page, Wrapper, Bar styled component
    - add left, right setting for desktop view
    - set width variable
    - change main page width to 1005v (test main component and sidebar position)
    
     On branch feature/#8
     Changes to be committed:
    	modified:   components/Layout/Layout.tsx
    	modified:   components/Layout/SideBar/SideBar.tsx
    	modified:   styles/Home.module.css

--- components/Layout/SideBar/SideBar.tsx
+++ components/Layout/SideBar/SideBar.tsx
@@ -1,12 +1,45 @@
 import React from "react";
 import styled from "styled-components";
 
-export const SideBar: React.FC = () => {
+//Types
+export const SIDE_BAR_POSITION_ENUM = {
+  left: `left: 0px;`,
+  right: `right: 0px;`,
+};
+
+export type SideBarPositionType = keyof typeof SIDE_BAR_POSITION_ENUM;
+//Interfaces
+export interface SideBarProps {
+  position: SideBarPositionType;
+  width?: string;
+}
+
+export const Page = styled.div`
+  width: ${(props: SideBarProps) => props.width};
+`;
+
+export const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  z-index: 0;
+`;
+
+export const Bar = styled.div`
+  position: fixed;
+  ${(props: SideBarProps) => SIDE_BAR_POSITION_ENUM[props.position]};
+  width: ${(props: SideBarProps) => props.width};
+`;
+
+const SideBar: React.FC<SideBarProps> = ({ position, width = "320px" }) => {
   return (
-    <div id="wrapper">
-      <div id="sidebar">
-        <>children</>
-      </div>
-    </div>
+    <Page position={position} width={width}>
+      <Wrapper>
+        <Bar position={position} width={width}>
+          <>SideBar Contents is here</>
+        </Bar>
+      </Wrapper>
+    </Page>
   );
 };
+
+export default SideBar;
